1946e9690a254fe05d9e33ed2c912ca52941bce5
Albert Maršík <albert.marsik@gmail.com>
2024-01-03 07:14:02 +0100

    frontend, todo integrate with server

--- crate_web/src/main.rs
+++ crate_web/src/main.rs
@@ -1,9 +1,12 @@
-#[macro_use]
-extern crate rocket;
 use std::collections::HashMap;
 
+#[macro_use]
+extern crate rocket;
 use rocket::form::Form;
 use rocket::response::content::RawHtml;
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+
 mod get_template;
 use get_template::get_template;
 
@@ -13,12 +16,25 @@ struct LoginForm {
     password: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Message {
+    message: String,
+}
+
 #[rocket::post("/submit", data = "<form>")]
 fn submit(form: Form<LoginForm>) -> RawHtml<String> {
-    println!("got {} and {}", form.username, form.password);
+    println!("pass {}", form.password);
     let mut data = HashMap::new();
     data.insert("nickname".to_string(), form.username.clone());
     get_template("chat", Some(data))
+}
+
+#[rocket::post("/receive_message", data = "<message>")]
+fn receive_message(message: Json<Message>) -> Json<Message> {
+    let response_text = message.0.message.to_uppercase();
+    Json(Message {
+        message: response_text,
+    })
 }
 
 #[rocket::get("/")]
@@ -28,5 +44,5 @@ fn index() -> RawHtml<String> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, submit])
+    rocket::build().mount("/", routes![index, submit, receive_message])
 }
